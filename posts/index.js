5a77208ea7b33b946c17c57c376f287b3830f509
VodeniZeko <saletovic.developer@gmail.com>
2024-04-14 21:39:34 -0500

    add posts and comments / sync implementation in MC style [not recommended]

---
+++ posts/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+
+const PORT = 4000;
+
+const app = express();
+app.use(bodyParser.json());
+// Mock data
+const posts = {};
+
+app.get("/posts", (req, res) => {
+	res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+	const id = randomBytes(4).toString("hex");
+	const { title } = req.body;
+
+	posts[id] = { id, title };
+
+	res.status(201).send(posts[id]);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port: ${PORT}`);
+});
