b02db6875907ab11b226997eec09dad871ea6a99
VodeniZeko <saletovic.developer@gmail.com>
2024-04-30 19:36:47 -0500

    add clusterIPs to communicate betwen pods (posts > eventBus > posts, to start and test with postman)

--- posts/index.js
+++ posts/index.js
@@ -24,7 +24,7 @@ app.post("/posts", async (req, res) => {
 	posts[id] = { id, title };
 
 	try {
-		const response = await axios.post("http://localhost:4005/events", {
+		const response = await axios.post("http://event-bus-srv:4005/events", {
 			type: "PostCreated",
 			data: {
 				id,
